64ae76dfb00af184187b6f136480c46983b02c46
alex.breger <alexbreger123@gmail.com>
2024-09-17 18:29:58 +0300

    Changes to some variables

--- system_programming/VariableSizeAllocator/vsa.c
+++ system_programming/VariableSizeAllocator/vsa.c
@@ -32,15 +32,15 @@ vsa_t* VsaInit(size_t total_memory, void* pool_memory)
 
 void* VsaAlloc(vsa_t* vsa, size_t memory_size) 
 {
-	size_t* current;
-	size_t* next;
+	size_t* current_block;
+	size_t* next_block;
 	size_t next_block_size;
-	int next_is_allocated;
+	int is_next_allocated;
 	size_t* best_fit_block = NULL;
 	size_t best_size_block = (size_t) - 1;
 	size_t remaining_size;
 	size_t block_size;
-	int is_allocated;
+	int is_current_allocated;
 	size_t size_metadata = sizeof(size_t);
 
 	if(!vsa || 0 == memory_size)
@@ -49,47 +49,48 @@ void* VsaAlloc(vsa_t* vsa, size_t memory_size)
 		return NULL;
 	}
 
-	current = (size_t*)vsa->pool_start;
+	current_block = (size_t*)vsa->pool_start;
 	remaining_size = vsa->total_size;
 
 	while(remaining_size > size_metadata)
 	{
-		block_size = *current & ~0x1;
-		is_allocated = *current & 0x1;
-		next = (void*)((char*)current + block_size);
+		block_size = *current_block & ~0x1;
+		is_current_allocated = *current_block & 0x1;
+		next_block = (void*)((char*)current_block + block_size);
 
-		if(0 == is_allocated && remaining_size > block_size)
+		if(0 == is_current_allocated && remaining_size > block_size)
 		{
-			next_block_size = *next & ~0x1;
-			next_is_allocated = *next & 0x1;
-			if(0 == next_is_allocated)
+			next_block_size = *next_block & ~0x1;
+			is_next_allocated = *next_block & 0x1;
+			if(0 == is_next_allocated)
 			{
-				*current = block_size + next_block_size;
+				*current_block = block_size + next_block_size;
 				continue;
 			}
 		}
 
-		current = next;
+		current_block = next_block;
 		remaining_size -= block_size;
 	}
 
-	current = (size_t*)vsa->pool_start;
+	current_block = (size_t*)vsa->pool_start;
     remaining_size = vsa->total_size;
 
 	while(remaining_size > 0)
 	{
-		block_size = *current & ~0x1;
-		is_allocated = *current & 0x1;
+		block_size = *current_block & ~0x1;
+		is_current_allocated = *current_block & 0x1;
 
-		if(0 == is_allocated && block_size >= memory_size)
+		if(0 == is_current_allocated && block_size >= memory_size)
 		{
 			if(block_size < best_size_block)
 			{
-				best_fit_block = current;
+				best_fit_block = current_block;
 				best_size_block = block_size;
 			}
 		}
-		current = (size_t*)((char*)current + block_size);
+		
+		current_block = (size_t*)((char*)current_block + block_size);
 		remaining_size -= block_size;
 	}
 
@@ -99,13 +100,14 @@ void* VsaAlloc(vsa_t* vsa, size_t memory_size)
 		
 		if(remaining_block_size >= size_metadata + size_metadata)
 		{
-			next = (size_t*)((char*)best_fit_block + memory_size + size_metadata);
+			next_block = (size_t*)((char*)best_fit_block + memory_size + size_metadata);
 			best_fit_block = memory_size | 0x1;
 		}
 		else
 		{
 			*best_fit_block |= 0x1;
 		}
+
 		return (void*)((char*)best_fit_block + size_metadata);
 	}
 	
@@ -153,8 +155,6 @@ size_t VsaLargestChunkAvailable(vsa_t* vsa)
 		is_current_allocated = *current_block & 0x1;
 		next_block = (void*)((char*)current_block + current_block_size);
 
-
-
 		if(0 == is_current_allocated)
 		{
 			if(current_block_size > largest_free_block)
@@ -173,12 +173,11 @@ size_t VsaLargestChunkAvailable(vsa_t* vsa)
 				*current_block = current_block_size + next_block_size;
 				continue;
 			}
-
-			current_block = (size_t*)((char*)current_block + current_block_size);
-			remaining_size -= current_block_size;
 		}
-			
+		
+		current_block = (size_t*)((char*)current_block + current_block_size);
+		remaining_size -= current_block_size;	
 	}
-	
+
 	return largest_free_block;
 }

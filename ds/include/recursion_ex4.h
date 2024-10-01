2bb6574d0872b4650e9234528a6d61502fde5e8e
alex.breger <alexbreger123@gmail.com>
2024-10-01 10:22:09 +0300

    Finished ex3 recursion

---
+++ ds/include/recursion_ex4.h
@@ -0,0 +1,12 @@
+#ifndef __RECURSION_EX4_H__
+#define __RECURSION_EX4_H__
+
+#include <stddef.h> /*size_t*/
+
+int StrCmp(const char *str1, const char *str2);
+size_t StrLen(const char *str);
+char *StrCat(char *dst, const char *src);
+char *StrCpy(char *dst, const char *src);
+char *StrStr(const char *haystack, const char *needle);
+
+#endif /*__RECURSION_EX4_H__*/
---
+++ ds/src/recursion_ex4.c
@@ -0,0 +1,94 @@
+// Alex Breger
+// Code Review Ben Bortkevich 6.8.24
+
+#include <assert.h> /*assert*/
+#include "strings_reimplementation.h" /*StrLen StrCmp*/
+#include "customStrings.h"
+
+int main()
+{
+const char *string1 = "abc";
+const char *string2 = "abc";
+const char *string3 = "acd";
+
+return 0;
+}
+
+size_t StrLen(const char *str)
+{
+	if('\0' == *str)
+    {
+        return 1;
+    }
+    
+	return 1 + StrLen(++str);
+}
+
+int StrCmp(const char *string1, const char *string2)
+{
+	
+	if((*string1 != *string2) || ('\0' == *string1 || '\0' == *string2))
+	{
+		return (int *) (*string1 - *string2);
+	}
+    
+	return StrCmp(++string1,++string2);
+}
+
+char *StrCat(char *dst, const char *src)
+{
+
+	if('\0' != *dst)
+	{
+		StrCat(dst + 1, src);
+	}
+	else
+	{
+		while('\0' != *src)
+		{
+			*dst = *src;
+			++dst;
+			++src;
+		}
+
+		*dst = '\0';
+	}
+
+	return dst;
+}
+
+char *StrStr(const char *haystack, const char *needle)
+{
+		
+	if('\0' == *needle)
+	{
+		return (char*)haystack;
+	}
+
+	if('\0' == *haystack)
+	{
+		return NULL;
+	}
+	
+	if(0 == StrNCmp(haystack, needle, StrLen(needle)))
+		{
+			return (char*)haystack;
+		}
+	
+	return StrStr(haystack + 1, needle);
+}
+
+char *StrCpy(char *dst, const char *src)
+{
+	
+	if('\0' == *src)
+	{
+		*dst = '\0';
+	}
+
+	*dst = *src;
+
+	StrCpy(dst + 1, src + 1);
+
+	return dst;
+}

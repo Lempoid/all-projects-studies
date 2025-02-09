a0b42c183cee8a1ee59f9d8c67cec55c7435b216
alex.breger <alexbreger123@gmail.com>
2025-02-09 12:52:59 +0200

    ulpoaded web exploitation mds

---
+++ cyber/network_based_defence/server2/server2.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello from Server 2)
+
+if __name__ == "__main__":
+    server = HTTPServer(('0.0.0.0', 8082), SimpleHTTPRequestHandler)
+    server.serve_forever()
